5fdb822cab997ba0bba8f1eee1317377fec42dda
marco antonio <2mbetwin@gmail.com>
2026-05-12 15:41:29 -0300

    criando as models e colocandoo alguns dados mockados de produtos no bd

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class Product(models.Model):
 
 class Client(models.Model):
     name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=20)
+    age = models.CharField(max_length=3)
 
     def __str__(self):
         return self.name
